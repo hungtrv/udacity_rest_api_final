d1030bb4e84625b6fac2774e47b4d8be328f5c42
Hung Tran <hungtrv@gmail.com>
2017-01-21 02:25:40 -0800

    Add USERS model

--- manage.py
+++ manage.py
@@ -25,6 +25,15 @@ manager.add_command('shell', Shell(make_context=_make_context))
 @manager.command
 def init_db():
 	db.create_all()
+	print "DB initialized!"
+
+@manager.command
+def add_user(username, password):
+	user = User(username=username)
+	user.set_password_hash(password)
+	db.session.add(user)
+	db.session.commit()
+	print "Added {0}!".format(user.username)
 
 
 if __name__ == "__main__":
