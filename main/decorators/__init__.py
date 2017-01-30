27c11b3cbb5d7bdbd2ff06435b8cc9768c29c4df
Hung Tran <hungtrv@gmail.com>
2017-01-29 22:59:31 -0800

    Add HTTP caching decorators cache_control, no_cache, etag

--- main/decorators/__init__.py
+++ main/decorators/__init__.py
@@ -1,3 +1,6 @@
 from .json import json
 from .paginate import paginate
 from .rate_limit import rate_limit
+from .caching import cache_control
+from .caching import no_cache
+from .caching import etag
