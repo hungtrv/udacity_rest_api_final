554b3d9d0d8289c5c6ded2e395c8476fccf1c1ad
Hung Tran <hungtrv@gmail.com>
2017-02-02 23:24:10 -0800

    Clean up unused import

--- main/__init__.py
+++ main/__init__.py
@@ -1,8 +1,8 @@
 import logging
 
 from flask import Flask
-from flask import Blueprint
-from flask import g
+# from flask import Blueprint
+# from flask import g
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from flask_httpauth import HTTPBasicAuth
