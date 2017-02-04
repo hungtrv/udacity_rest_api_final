6600a84de2fef1c536d36ef2f61030cabd7e6589
Hung Tran <hungtrv@gmail.com>
2017-02-04 03:44:18 -0800

    Add proposals model

--- main/models/__init__.py
+++ main/models/__init__.py
@@ -1,4 +1,5 @@
 __all__ = [
 	'users',
-	'requests'
+	'requests',
+	'proposals'
 ]
---
+++ main/models/proposals.py
@@ -0,0 +1,42 @@
+from flask import url_for
+from main import db
+from main.errors import ValidationError
+from main.models.base import TimestampMixin
+
+class Proposal(db.Model, TimestampMixin):
+	__tablename__ = 'proposals'
+	id = db.Column(db.Integer, primary_key=True)
+	request_id = db.Column(db.Integer, db.ForeignKey('requests.id'), index=True)
+	user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+	accepted = db.Column(db.Boolean)
+
+
+	def get_url(self):
+		return url_for('api.get_proposal', id=self.id, _external=True)
+
+
+	def export_data(self):
+		return {
+			'self_url': self.get_url(),
+			'user_url': self.user.get_url(),
+			'request_url': self.request.get_url(),
+			'accepted': self.filled,
+		}
+
+
+	def import_data(self, data):
+		try:
+			pass
+		except KeyError as e:
+			raise ValidationError('Invalid user: missing ' + e.args[0])
+
+		return self
+
+
+	def update_data(self, data):
+		fields = ['accepted']
+		for field in fields:
+			if field in data:
+				setattr(self, field, data[field])
+		
+		return self
